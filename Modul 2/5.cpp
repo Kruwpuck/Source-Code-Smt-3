da7add6a4777f7889122b4fa8397ba0d6fd44cad
Habb <ihabhasanainakmal0409@gmail.com>
2024-09-21 19:10:13 +0700

    Minggu 2

--- Modul 2/5.cpp
+++ Modul 2/5.cpp
@@ -1,17 +1,39 @@
 #include <iostream>
+#define max 10
+
 using namespace std;
 
-const nMax = 51;
-
+const int nMax = 51;
 struct mahasiswa {
-    string nama,NIM;
+    string NIM, nama;
     int nilai;
 };
 
-mahasiswa siswa[nMax];
-
-int nilaiPertama(siswa T, int N, string NIM)
+mahasiswa arrayMahasiswa[nMax];
+int nilaiPertama(mahasiswa arrayMahasiswa[] , int N, string NIM){
+    for (int i=0; i < N ;i++){
+        if (arrayMahasiswa[i].NIM == NIM){
+            return i;
+        }
+    }
+    return -1;
+}
 
 int main(){
-
+    int N,i;
+    string NIM;
+    cin >> N;
+    for (i=0;i<N;i++){
+        cin>>arrayMahasiswa[i].NIM;
+        cin>>arrayMahasiswa[i].nama;
+        cin>>arrayMahasiswa[i].nilai;
+    }
+    cin >> NIM;
+    if (nilaiPertama(arrayMahasiswa,N,NIM)==-1){
+        cout << "Tidak ditemukan";
+    }else{
+        cout << arrayMahasiswa[nilaiPertama(arrayMahasiswa,N,NIM)].nama << endl;
+        cout << arrayMahasiswa[nilaiPertama(arrayMahasiswa,N,NIM)].NIM << endl;
+        cout << arrayMahasiswa[nilaiPertama(arrayMahasiswa,N,NIM)].nilai << endl;
+    }
 }
